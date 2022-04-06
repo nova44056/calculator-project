a9ed5e11df6455724f44b582473ede6d7796a595
Uy Seng <nova@Uys-MacBook-Air.local>
2022-04-06 16:46:34 -0500

    first commit

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,35 +1,81 @@
 class Calculator {
-    firstNumber;
-    secondNumber;
-    operator;
-    value;
+  firstNumber;
+  secondNumber;
+  operator;
+  value;
 
-    constructor(firstNumber, secondNumber, operator) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        this.operator = operator;
-        this.value = 0;
+  constructor(firstNumber, secondNumber, operator) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+    this.operator = operator;
+    this.value = 0;
+  }
+
+  // determine the current operation's label
+  getAction() {
+    switch (this.operator) {
+      case "add":
+        return "adding";
+      case "subtract":
+        return "subtracting";
+      case "multiply":
+        return "multiplying";
+      case "divide":
+        return "dividing";
+      default:
+        return "";
     }
+  }
 
-    // determine the current operation's label
-    getAction() {}
+  // perform a calculation based on the currently selected operation
+  operate() {
+    switch (this.operator) {
+      case "add":
+        this.add();
+        break;
+      case "subtract":
+        this.subtract();
+        break;
+      case "multiply":
+        this.multiply();
+        break;
+      case "divide":
+        if (this.secondNumber === 0) {
+          this.attemptedDivideByZero = true;
+        } else {
+          this.divide();
+        }
+        break;
+      default:
+        break;
+    }
+  }
 
-    // performa a calculation based on the currently selected operation
-    operate() {}
+  attemptedDivideByZero = false;
 
-    attemptedDivideByZero = false;
+  // perform addition operation
+  add() {
+    this.value = this.firstNumber + this.secondNumber;
+  }
 
-    // perform addition operation
-    add() {}
+  // perform subtraction operation
+  subtract() {
+    this.value = this.firstNumber - this.secondNumber;
+  }
 
-    // perform subtraction operation
-    subtract() {}
+  // perform multiplication operation
+  multiply() {
+    this.value = this.firstNumber * this.secondNumber;
+  }
 
-    // perform multiplication operation
-    multiply() {}
-
-    // perform divide operation
-    divide() {}
+  // perform divide operation
+  divide() {
+    if (this.secondNumber === 0) {
+      this.attemptedDivideByZero = true;
+    } else {
+      this.value = this.firstNumber / this.secondNumber;
+    }
+  }
 }
 
 //DO NOT TOUCH THIS LINE OF CODE//
--- calculator/controller.js
+++ calculator/controller.js
@@ -1,20 +1,87 @@
+window.onload = () => {
+  // focus on the first number input on page load
+  document.getElementById("first_number").focus();
+};
+
 // setup variables and get a calculator instance
-function CreateCalculator() {}
+function CreateCalculator(first_number, second_number, operator) {
+  return new Calculator(first_number, second_number, operator);
+}
 
 // perform a calculation when the operator button is clicked
-function calculate() {}
+function calculate() {
+  // get first and second number
+  const first_number = parseFloat(
+    document.getElementById("first_number").value
+  );
+
+  const second_number = parseFloat(
+    document.getElementById("second_number").value
+  );
+
+  if (isNaN(first_number) || isNaN(second_number)) {
+    updateResultText("Please enter a valid number");
+    // // clear the result text after 2 seconds
+    setTimeout(() => updateResultText(""), 2000);
+    return;
+  }
+
+  // get operator
+  const operator = getOperator();
+
+  // create a calculator instance
+  const calculator = CreateCalculator(first_number, second_number, operator);
+  // do the calculation
+  calculator.operate();
+
+  if (calculator.attemptedDivideByZero) {
+    updateResultText("Cannot divide by zero");
+    return;
+  }
+
+  updateResultText(
+    `The result of ${calculator.getAction()} ${first_number} and ${second_number} is ${
+      calculator.value
+    }`
+  );
+}
 
 /**
  * set the text in the result section of the UI
  * @param {*} value
  */
-function updateResultText(value) {}
+function updateResultText(value) {
+  document.getElementById("result").innerHTML = value;
+}
 
 // should clear input text values and focus the first number input
-function clearValues() {}
+function clearValues() {
+  // clear first_number
+  document.getElementById("first_number").value = "";
+  // clear second_number
+  document.getElementById("second_number").value = "";
+
+  // reset operator to default
+  document.getElementById("add").checked = true;
+
+  // focus to the first number input on clearing
+  document.getElementById("first_number").focus();
+}
 
 /**
  * get the selected operator from the UI
  * @returns Operators
  */
-function getOperator() {}
+function getOperator() {
+  // default
+  var operator = "add";
+  // select the operator class from the UI
+  Array.from(document.getElementsByClassName("operator")).forEach((e) => {
+    const operator_input = e.firstElementChild;
+    // if the input box is checked
+    if (operator_input.checked) {
+      operator = operator_input.value;
+    }
+  });
+  return operator;
+}
